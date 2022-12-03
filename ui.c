2b107fb51f960c34620afe6d6e983ef767441441
Hikmat Jafarli <jafarlihi@gmail.com>
2022-12-03 05:59:47 +0400

    Add stuff

--- ui.c
+++ ui.c
@@ -5,6 +5,7 @@
 #include <curses.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <string.h>
 
 int termx, termy, topbarx, topbary, bottombarx, bottombary;
 WINDOW *topbar, *bottombar;
@@ -61,6 +62,41 @@ void draw_objdump(void) {
   wrefresh(topbar);
 }
 
+void draw_help(void) {
+  move(termy - 1, 2);
+  printw("%s", "'q' to quit | 's' to single-step | 'r' to run");
+}
+
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0') 
+
+void draw_registers(void) {
+  char *output = calloc(10000, sizeof(char));
+  wmove(bottombar, 1, 1);
+  sprintf(output, "AX: "BYTE_TO_BINARY_PATTERN" | BX: "BYTE_TO_BINARY_PATTERN" | CX: "BYTE_TO_BINARY_PATTERN" | DX: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(emulator->state->ax), BYTE_TO_BINARY(emulator->state->bx), BYTE_TO_BINARY(emulator->state->cx), BYTE_TO_BINARY(emulator->state->dx));
+  wprintw(bottombar, "%s\n", output);
+  wmove(bottombar, 2, 1);
+  sprintf(output, "SP: "BYTE_TO_BINARY_PATTERN" | BP: "BYTE_TO_BINARY_PATTERN" | SI: "BYTE_TO_BINARY_PATTERN" | DI: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(emulator->state->sp), BYTE_TO_BINARY(emulator->state->bp), BYTE_TO_BINARY(emulator->state->si), BYTE_TO_BINARY(emulator->state->di));
+  wprintw(bottombar, "%s\n", output);
+  wmove(bottombar, 3, 1);
+  sprintf(output, "CS: "BYTE_TO_BINARY_PATTERN" | DS: "BYTE_TO_BINARY_PATTERN" | SS: "BYTE_TO_BINARY_PATTERN" | ES: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(emulator->state->cs), BYTE_TO_BINARY(emulator->state->ds), BYTE_TO_BINARY(emulator->state->ss), BYTE_TO_BINARY(emulator->state->es));
+  wprintw(bottombar, "%s\n", output);
+  wmove(bottombar, 4, 1);
+  sprintf(output, "IP: "BYTE_TO_BINARY_PATTERN" | Flags: "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(emulator->state->ip), BYTE_TO_BINARY(emulator->state->flags));
+  wprintw(bottombar, "%s\n", output);
+  wborder(bottombar, 0, 0, 0, 0, 0, 0, 0, 0);
+  draw_help();
+  wrefresh(bottombar);
+}
+
 void get_window_dimensions() {
   getmaxyx(stdscr, termy, termx);
   topbarx = termx;
@@ -74,8 +110,7 @@ void get_window_dimensions() {
 void draw_window(WINDOW* win, int height, int width) {
   if (win == bottombar) {
     wborder(win, 0, 0, 0, 0, 0, 0, 0, 0);
-    move(termy - 1, 2);
-    printw("%s", "'q' to quit | 's' to single-step | 'r' to run");
+    draw_help();
   }
   wrefresh(win);
   refresh();
@@ -89,6 +124,9 @@ void resize_handler(int sig) {
   draw_window(topbar, topbary, topbarx);
   draw_window(bottombar, bottombary, bottombarx);
   draw_objdump();
+  draw_registers();
+  draw_help();
+  refresh();
 }
 
 void init_ui(Emulator *emu) {
@@ -103,6 +141,7 @@ void init_ui(Emulator *emu) {
   draw_window(bottombar, bottombary, bottombarx);
 
   draw_objdump();
+  draw_registers();
 
   while (true) {
     c = 0;
